458c24defd8be15f8257be5a3e4a0bee8db74e3a
alvdelci <rafithydev@gmail.com>
2022-06-09 00:45:36 -0300

    Adicionando base do chatbot

--- src/pages/map/Map.js
+++ src/pages/map/Map.js
@@ -1,44 +1,63 @@
 import React from "react";
 
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 import Bar from "../../componentes/bar/Bar";
 
-import './styles.css';
+import "./styles.css";
+
+//chatbot imports
+import Chatbot from "react-chatbot-kit";
+import "react-chatbot-kit/build/main.css";
+import config from "../../componentes/chatbot/config";
+import messageParser from "../../componentes/chatbot/MessageParser";
+import actionProvider from "../../componentes/chatbot/ActionProvider";
 
 function Map() {
-
-    return (
-        <>
-            <Bar />
-            <MapContainer
-                id="map-container"
-                center={[-12.673960, -39.102010]}
-                zoom={13}
-                minZoom={3}
-                maxBounds={[
-                    //south west
-                    [-180.000000, 180.000000],
-                    //north east
-                    [180.000000, -180.000000]
-                ]}
-                maxBoundsViscosity={1.0}
-            >
-
-
-                <TileLayer
-                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-
-                />
-                <Marker position={[-12.673960, -39.102010]}>
-                    <Popup>
-                        Informações do ponto. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
-            </MapContainer>
-        </>
-    );
+  return (
+    <>
+      <Bar />
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          height: "100px",
+          zIndex: 1001,
+        }}
+      >
+        <Chatbot
+          disableScrollToBottom
+          config={config}
+          messageParser={messageParser}
+          actionProvider={actionProvider}
+        />
+      </div>
+      <MapContainer
+        id="map-container"
+        center={[-12.67396, -39.10201]}
+        zoom={13}
+        minZoom={3}
+        maxBounds={[
+          //south west
+          [-180.0, 180.0],
+          //north east
+          [180.0, -180.0],
+        ]}
+        maxBoundsViscosity={1.0}
+      >
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[-12.67396, -39.10201]}>
+          <Popup>
+            Informações do ponto. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </>
+  );
 }
 
 export default Map;
