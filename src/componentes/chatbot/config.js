2dd6c90b3d37492d2bfbd86ef2f7e4b9ca529895
alvdelci <rafithydev@gmail.com>
2022-06-09 01:03:06 -0300

    Adicionando novas configurações ao bot

--- src/componentes/chatbot/config.js
+++ src/componentes/chatbot/config.js
@@ -1,7 +1,19 @@
+// in config.js
 import { createChatBotMessage } from "react-chatbot-kit";
 
+const botName = "Dona Maria";
+
 const config = {
-  initialMessages: [createChatBotMessage(`Hello world`)],
+  initialMessages: [createChatBotMessage(`Hi! I'm ${botName}`)],
+  botName: botName,
+  customStyles: {
+    botMessageBox: {
+      backgroundColor: "#FF8C00",
+    },
+    chatButton: {
+      backgroundColor: "orange",
+    },
+  },
 };
 
 export default config;
